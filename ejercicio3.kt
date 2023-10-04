cf27675306287539e33a22a15d3890a95c899167
AVLWidDarknes <alejandrovillarrealupiicsa@gmail.com>
2023-10-04 14:29:44 -0600

    Esta es la tarea 3

---
+++ ejercicio3.kt
@@ -0,0 +1,12 @@
+fun main() {
+    
+    val x2 = 4f;
+    val y2 = 3f;
+    val x1 = -3f;
+    val y1 = -2f;
+    
+    val pendiente:Float = (y2-y1)/(x2-x1);
+    
+    println("La pendiente tiene un valor de $pendiente");
+    
+}

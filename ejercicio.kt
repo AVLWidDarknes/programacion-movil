0be6fc9087e5109710b387041005ba250e380973
AVLWidDarknes <alejandrovillarrealupiicsa@gmail.com>
2023-10-04 14:22:34 -0600

    Este es la primer tarea (:

---
+++ ejercicio.kt
@@ -0,0 +1,8 @@
+//Villarreal Reyes Michell Alejandro
+fun main() {
+    
+    println("Hello UPIICSA");
+    println("Esta es la primera evidencia");
+    print("Materia de sexto semestre, este es un println");
+    print("Este es un print");
+}
